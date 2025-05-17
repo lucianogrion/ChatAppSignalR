1443e68166ffa9d810785207ddb85bc13e89a204
Lgrion <82838993+lucianogrion@users.noreply.github.com>
2025-05-17 20:00:53 -0300

    Add files via upload

---
+++ ChatAppSignalR/Program.cs
@@ -0,0 +1,13 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// 🔴 Agrega servicios ANTES de Build()
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+
+app.MapGet("/", () => Results.Redirect("/index.html"));
+app.MapHub<ChatHub>("/chathub");
+
+app.Run();
